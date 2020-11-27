6c82174f593cc2aff5cb8f638a7e16175b6de2bf
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-11-27 12:10:59 +0700

    Update pingCommand.ts

--- src/commands/util/pingCommand.ts
+++ src/commands/util/pingCommand.ts
@@ -8,7 +8,7 @@ export default {
   description: "show bot ping",
   execute(message, args, client) {
     const embed = new MessageEmbed()
-        .addField("Pongg!! :ping_pong:",  Math.round(client.ws.ping) + "ms", true)
+        .addField("Pong!",  Math.round(client.ws.ping) + "ms", true)
         .setColor(client.color)
      message.channel.send(embed)
   }
