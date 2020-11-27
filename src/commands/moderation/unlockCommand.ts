1ad00788cd14dff61ec3a3477cf6c05747ce1f10
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-11-28 00:18:44 +0700

    Create unlockCommand.ts

---
+++ src/commands/moderation/unlockCommand.ts
@@ -0,0 +1,27 @@
+import { MessageEmbed } from "discord.js";
+
+export default {
+  name: "unlock",
+  aliases: ["unlockdown"],
+  cooldown: 5,
+  guildOnly:true,
+  description: "Unlock a locked channel",
+  execute(message, args, client) {
+
+   let noPerms = new MessageEmbed()
+   .setColor(client.color)
+   .setDescription(`**You must have the \`MANAGE_CHANNELS\` permission**`);
+   if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send(noPerms);
+
+   let clientNoPerms = new MessageEmbed()
+   .setColor(client.color)
+   .setDescription(`**I need to have the \`MANAGE_CHANNELS\` permission**`);
+   if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send(clientNoPerms);
+
+
+   let Succes = new MessageEmbed()
+   .setColor(client.color)
+   .setDescription(`**\`${message.author.tag}\` has unlocked the channel`)
+   return message.channel.send(Succes)
+ }
+}
