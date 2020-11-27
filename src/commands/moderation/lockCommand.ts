e425a8cad37fbecd620889b8be251a2a434b7358
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-11-27 22:19:33 +0700

    Update lockCommand.ts

--- src/commands/moderation/lockCommand.ts
+++ src/commands/moderation/lockCommand.ts
@@ -1,3 +1,21 @@
 import { MessageEmbed } from "discord.js";
 
+export default {
+  name: "lock",
+  aliases: ["lockdown"],
+  description: "Lock channel",
+  execute(client, args, message) {
 
+  if(!message.guild.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send({embed:{description: "**You do not have permission to lock this channel**", color:client.color}});
+  
+   message.channel.overwritePermissions([{
+        id: message.guild.id,
+        deny : ['SEND_MESSAGES'],
+     },
+    ],
+   );
+
+  return msg.channel.send({embed:{description:`**\`${message.author.tag}\` has locked this channel**`, color:client.color}});
+
+ }
+}
