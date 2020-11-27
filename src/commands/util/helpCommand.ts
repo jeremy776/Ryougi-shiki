603a9ad4f8e1b6db9da1e40bf89851cf42a55729
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-11-28 01:00:18 +0700

    Update helpCommand.ts

--- src/commands/util/helpCommand.ts
+++ src/commands/util/helpCommand.ts
@@ -37,6 +37,7 @@ export default {
     }
 
    let cooldown = cmd.cooldown+"s";
+   if(cooldown == undefined) cooldown = "None";
    if(!cooldown) cooldown = "None";
 
     const embed = new MessageEmbed()
