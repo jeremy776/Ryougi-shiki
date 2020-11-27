b330c3009bcde89978882d3ba5bc4ced1bcd231a
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-11-27 14:33:40 +0700

    Update evalCommand.ts

--- src/commands/developer/evalCommand.ts
+++ src/commands/developer/evalCommand.ts
@@ -1,1 +1,12 @@
-import "Discord" from "discord.js";
+import { MessageEmbed } from "discord.js";
+
+export default {
+  name: "eval",
+  aliases: ["ev", "v"],
+  guildOnly: false,
+  description: "Evalute Code",
+  execute(message, args, client) {
+
+
+ }
+}
