4f8ce0c35cdcd24cb0586f274b401b224f324615
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-11-27 22:33:39 +0700

    Update lockCommand.ts

--- src/commands/moderation/lockCommand.ts
+++ src/commands/moderation/lockCommand.ts
@@ -8,9 +8,10 @@ export default {
   description: "Lock channel",
   execute(client, args, message) {
 
-  if(!message.guild.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send({embed:{description: "**You do not have permission to lock this channel**", color:client.color}});
-  
-   message.channel.overwritePermissions([{
+  if(!message.guild.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send({embed:{description: "**You do not have permission \`MANAGE_CHANNELS\` to lock this channel**", color:client.color}});
+  if(!message.guild.member.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send({embed:{description: "**Give me \`MANAGE_CHANNELS\` permission if you want to run this command**", color:client.color}});
+   
+  message.channel.overwritePermissions([{
         id: message.guild.id,
         deny : ['SEND_MESSAGES'],
      },
