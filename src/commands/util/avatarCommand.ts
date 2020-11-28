32bb7ee8831dbcf6cd8b9eb535bdf233b179a0c1
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-11-28 22:16:01 +0700

    Update avatarCommand.ts

--- src/commands/util/avatarCommand.ts
+++ src/commands/util/avatarCommand.ts
@@ -13,17 +13,27 @@ export default {
     if(!userFind) userFind = message.author.username;
 
     if(typeof userFind == 'string') {
-       user = client.users.cache.find(x => x.username == userFind)
+       let a = client.users.cache.find(x => x.username == userFind)
+       let wrongUsername = new MessageEmbed()
+       .setDescription(`**The name you wrote was not detected**`)
+       .setColor(client.color)
+       if(a == undefined) return message.channel.send(wrongUsername)
+       user = a;
     }
     if(!isNaN(userFind)) {
-       user = client.users.cache.find(x => x.id == userFind)
+       let b = client.users.cache.find(x => x.id == userFind)
+       let wrongId = new MessageEmbed()
+       .setDescription(`**The user id that you wrote is invalid or not detected by me**`)
+       .setColor(client.color)
+       if(b == undefined) return message.channel.send(wrongId)
+       user = b;
     }
   }
 
 
   let avatar = new MessageEmbed()
   .setColor(client.color)
-  .setImage(user.displayAvatarURL({dynamic:true}))
+  .setImage(user.displayAvatarURL({dynamic:true, size:2048}))
   .setAuthor(`Avatar from ${user.tag}`)
   return message.channel.send(avatar)
 
