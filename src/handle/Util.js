cdf1d214db7a06a05abd4faf48119af5b9407cf6
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-11-28 20:59:29 +0700

    Create Util.js

---
+++ src/handle/Util.js
@@ -0,0 +1,24 @@
+class Util {
+
+ static cpu() {
+  let cpuUsage = process.cpuUsage();
+  return (cpuusage.user/cpuusage.system).toFixed(2) + "%";
+ }
+
+ static parseDur(ms) {
+   let S = ms / 1000;
+ 
+  let D = parseInt(S / 86400);
+  S = S % 86400;
+ 
+  let H = parseInt(S / 3600);
+  S = S % 3600;
+ 
+  let M = parseInt(S / 60);
+  S = parseInt(S % 60);
+ 
+  if (S || M || H || D) return `${D}D ${H}H ${M}M ${S}S`;
+ }
+}
+
+module.exports = Util;
