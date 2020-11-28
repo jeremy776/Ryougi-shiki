c6c3d44a93e8f2df1c2be89f597efa57152e8133
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-11-28 21:47:31 +0700

    Create avatarCommand.ts

---
+++ src/commands/util/avatarCommand.ts
@@ -0,0 +1,33 @@
+import { MessageEmbed } from "discord.js";
+
+export default {
+  name: "avatar",
+  aliases: ["av"],
+  cooldown: 2,
+  description: "Get the user avatar that is mentioned",
+  execute(message, args, client) {
+
+  let nameUser = args[0];
+  let user = message.mentions.users.first();
+  if(!user) user = nameUser;
+  
+  if(typeof nameUser == String) {
+   let username = client.users.cache.find(x => x.name == nameUser)
+
+   let wrongUserName = new MessageEmbed()
+   .setColor(client.color)
+   .setDescription(`**The name of the person you wrote is wrong**`)
+   if(!user) return message.channel.send(wrongUserName)
+
+   user = username;
+  }
+
+
+  let avatar = new MessageEmbed()
+  .setColor(client.color)
+  .setAvatar(user.displayAvatarURL({dynamic:true}))
+  .setAuthor(`Avatar from **\`${user.tag}\`**`)
+  return message.channel.send(avatar)
+
+  }
+ }
