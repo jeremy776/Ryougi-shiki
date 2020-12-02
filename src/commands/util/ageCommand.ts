48ed9b94f57e6f99079c2a02f380cef7688110f9
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-02 21:04:50 +0700

    Update ageCommand.ts

--- src/commands/util/ageCommand.ts
+++ src/commands/util/ageCommand.ts
@@ -25,7 +25,7 @@ export default {
 
 
  function checkAge(age) {
-  let data = moment(<any>new Date()).format("YYYY") - (<any>age)
+  let data = moment(+new Date()).format("YYYY") - age
   return data
  }
 
