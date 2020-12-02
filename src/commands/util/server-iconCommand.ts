220b1c79c5de8c0812ac9ac3762d77f554209315
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-02 19:08:59 +0700

    Create server-iconCommand.ts

---
+++ src/commands/util/server-iconCommand.ts
@@ -0,0 +1,20 @@
+import { MessageEmbed } from "discord.js";
+
+export default {
+  name: "server-icon",
+  aliases: ["si", "servericon", "iconserver"],
+  description: "Shows the server icon",
+  cooldown: 3,
+  execute(message, args, client) {
+
+  let embed = new MessageEmbed()
+  .setColor(client.color)
+  .setImage(message.guild.iconURL({dynamic:true, size:})
+  .setTimestamp()
+  .setDescription(message.guild.name)
+
+  return message.channel.send(embed)
+
+
+ }
+}
