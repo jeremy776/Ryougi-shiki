975e257c1571494dec58dad992b1b716388f5a6b
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-03 11:01:30 +0700

    Create snipeCommand.ts

---
+++ src/commands/util/snipeCommand.ts
@@ -0,0 +1,26 @@
+import { MessageEmbed } from "discord.js";
+
+export default {
+  name: "snipe",
+  cooldown: 3,
+  description: "Get messages that have been deleted before",
+  guildOnly:true,
+  execute(msg, args, client) {
+
+  let data = client.snipe.get(msg.channel.id);
+  let noData = new MessageEmbed()
+  .setDescription("**There are no messages deleted on this channel**")
+  .setColor(client.color)
+  .setTimestamp()
+  if(!data) return msg.channel.send(noData);
+
+  let succes = new MessageEmbed()
+  .setAuthor(data.author)
+  .setDescription(data.content)
+  .setColor(client.color)
+  .setTimestamp()
+  if(data.image) succes.setImage(data.image)
+  return msg.channel.send(succes);
+
+ }
+}
