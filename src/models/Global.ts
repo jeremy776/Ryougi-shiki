aa932cde48630744909ef73d2227df4eef08366b
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-04 13:50:52 +0700

    Update Global.ts

--- src/models/Global.ts
+++ src/models/Global.ts
@@ -1,4 +1,14 @@
 const mongoose = require("mongoose");
+
+mongoose.connect(client.config.mongoURL, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true
+})
+mongoose.connection.on("connected", (ngebuk) => {  
+  console.log("DATABASE TELAH DI KONEK BAPAK/IBU")
+  if(ngebuk) console.log(ngebuk)
+})
+
 const Schema = mongoose.Schema;
 
 let GlobalChat = new Schema({
