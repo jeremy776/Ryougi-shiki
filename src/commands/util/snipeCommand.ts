6553ba432e48b29360bd1dc13e971774f650d2ed
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-04 23:44:47 +0700

    Update snipeCommand.ts

--- src/commands/util/snipeCommand.ts
+++ src/commands/util/snipeCommand.ts
@@ -14,6 +14,20 @@ export default {
   .setTimestamp()
   if(!data) return msg.channel.send(noData).then(x => x.delete({timeout:4000}));
 
+  if(data.embeds[0]) {
+   let embed = data.embeds[0];
+   let embedMessage = new MessageEmbed()
+   .setAuthor(data.author.tag, data.author.displayAvatarURL({dynamic:true}))
+   if(embed.description) embedMessage.setDescription(embed.description)
+   if(embed.color) embedMessage.setColor(embed.color)
+   if(embed.thumbnail) embedMessage.setThumbnail(embed.thumbnail)
+   if(embed.fields.length > 0) {
+      embed.fields.map(x => embedMessage.addField(x.name, x.value))
+   }
+   return msg.channel.send(embedMessage)
+  }
+
+
   let succes = new MessageEmbed()
   .setAuthor(data.author.tag, data.author.displayAvatarURL({dynamic:true}))
   .setColor(client.color)
