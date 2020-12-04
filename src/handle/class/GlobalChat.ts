6a00483f3265491b756e148d523b52d195fcbc87
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-04 14:00:27 +0700

    Update GlobalChat.ts

--- src/handle/class/GlobalChat.ts
+++ src/handle/class/GlobalChat.ts
@@ -13,5 +13,14 @@ export default class Global {
    })
    return data;
   }
+ 
+ async newServer(guild, data, models) {
+  if(!guild) throw new Error('Guild not found');
+  if(!data) throw new Error('Data not found');
+  if(!models) throw new Error("Could Not Find Database Models!");
+  const db = Object.assign({ _id: mongoose.Types.ObjectId()}, data)
+  const Data = new models(db);
+  return Data.save()
+ }
   
 }
