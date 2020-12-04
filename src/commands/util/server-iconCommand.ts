84fb10b0e2ce0d2b5d4202c1b15f5c6eb4c041b9
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-05 00:58:54 +0700

    Update server-iconCommand.ts

--- src/commands/util/server-iconCommand.ts
+++ src/commands/util/server-iconCommand.ts
@@ -11,7 +11,7 @@ export default {
   .setColor(client.color)
   .setImage(msg.guild.iconURL({dynamic:true, size:2048}))
   .setTimestamp()
-  .setAuthor(msg.guild.name, msg.author.displayAvatarURL({dynamic:true})));
+  .setAuthor(msg.guild.name, msg.author.displayAvatarURL({dynamic:true}));
   return msg.channel.send(embed)
 
  }
