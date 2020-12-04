b7d50f3ce4a167cd7ea8afe588f564870c568ef1
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-04 13:44:44 +0700

    Update GlobalChat.ts

--- src/handle/class/GlobalChat.ts
+++ src/handle/class/GlobalChat.ts
@@ -1,1 +1,17 @@
 import { MessageEmbed } from "discord.js";
+const server = require('../../models/Global').default
+
+export default class Global {
+ guild:any
+  constructor(guild){
+   this.guild = guild; 
+  }
+  
+  async get(){
+   const data = server.findOne({
+     guild:this.guild
+   })
+   return data;
+  }
+  
+}
