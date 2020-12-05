e73e8a6ef3b2e01a89e57dd6af8943398b2aab26
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-06 02:47:35 +0700

    Update randomNumberCommand.ts

--- src/commands/util/randomNumberCommand.ts
+++ src/commands/util/randomNumberCommand.ts
@@ -1,11 +1,13 @@
 export default {
   name: "randomnumber",
   aliases: ["random-number", "number-generator"],
-  description: "random number",
+  description: "Get A Random Number\n• randomnumber [max]\n•default value: 50",
   execute(msg, args, client) {
 
   let max = args[0];
   if(!max) max = 50;
+
+  if(!isNaN(max)) return msg.reply("**Max Must Be A Number**")
 
   return msg.reply("I got the number\n"+client.util.randomNumber(1, max))
  }
