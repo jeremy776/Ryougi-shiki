dbc910e297b7c5d916ad16c90f97ec4ec7c8ae84
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-06 02:41:41 +0700

    Create randomNumberCommand.ts

---
+++ src/commands/util/randomNumberCommand.ts
@@ -0,0 +1,12 @@
+export default {
+  name: "randomNumber",
+  aliases: ["random-number", "number-generator"],
+  description: "random number",
+  execute(msg, args, client) {
+
+  let max = args[0];
+  if(!max) max = 50;
+
+  return msg.reply("I got the number\n"+client.util.randomNumber(1, max))
+ }
+}
