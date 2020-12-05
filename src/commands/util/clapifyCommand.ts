2130e30028ace6b9a64446274f4c3083bed2422c
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-06 01:40:01 +0700

    Create clapifyCommand.ts

---
+++ src/commands/util/clapifyCommand.ts
@@ -0,0 +1,17 @@
+export default {
+   name: "clapify",
+   cooldown:2,
+   description: "👏clapify👏text👏",
+   execute(msg, args, client) {
+
+   try {
+    args = args.join(" ") || "Input Text";
+    args = args.replace(/ /g, "👏");
+    args += "👏";
+    return msg.channel.send(`👏${args}`);
+  } catch (e) {
+    return msg.channel.send(`Oh no an error occured :( \`${e.message}\` try again later`);
+  }
+
+ }
+}
