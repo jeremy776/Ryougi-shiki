a76f94c15da2fd831640fdb1ee571f1027ff26ea
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-05 12:33:15 +0700

    Create penisCommand.ts

---
+++ src/commands/util/penisCommand.ts
@@ -0,0 +1,19 @@
+import { MessageEmbed } from "discord.js";
+
+export default {
+  name: "penis",
+  cooldown: 5,
+  description: "how long is your penis?",
+  guildOnly:true,
+  execute(msg, args, client) {
+
+  let user = msg.mentions.users.first() || msg.author;
+  let embed = new MessageEmbed()
+  .setColor(client.color)
+  .setAuthor(`${user.username} Penis`)
+  .setDescription(`8`.repeat(client.util.randomNumber(1, 10))+`D`)
+  .setTimestamp()
+  return msg.channel.send(embed)
+
+ }
+}
