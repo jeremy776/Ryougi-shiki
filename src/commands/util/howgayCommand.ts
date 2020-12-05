7d263828e1bea0023130225f1aebc33cb6482220
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-05 20:06:46 +0700

    Create howgayCommand.ts

---
+++ src/commands/util/howgayCommand.ts
@@ -0,0 +1,19 @@
+import { MessageEmbed } from "discord.js";
+
+export default {
+  name: "howgay",
+  description: "you gay!",
+  cooldown: 3,
+  guildOnly:true,
+  execute(msg, args, client) {
+
+  let user = msg.mentions.users.first() || msg.author;
+  let embed = new MessageEmbed()
+  .setColor(client.color)
+  .setAuthor(user.tag + " is gay?", user.displayAvatarURL({dynamic:true}))
+  .setDescription(client.util.randomNumber(1, 100)+"%")
+  .setTimestamp()
+  return msg.channel.send(embed)
+
+ }
+}
