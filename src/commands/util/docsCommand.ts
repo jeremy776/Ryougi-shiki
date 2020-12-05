7f77e3fd5a57f32b9a736a3c11323556e0990e63
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-06 01:44:11 +0700

    Create docsCommand.ts

---
+++ src/commands/util/docsCommand.ts
@@ -0,0 +1,22 @@
+const fetch = require("node-fetch");
+
+export default {
+   name: "docs",
+   aliases: ["discord.js", "discordjs", "discord-js"],
+   description: "docs"
+   execute(msg, args, client) {
+
+   const search = args[0];
+  if (!search) return message.channel.send('Not found :(');
+  let version = args[1];
+  if (!version) version = `stable`;
+  
+  fetch(`https://djsdocs.sorta.moe/v1/main/${version}/embed?q=${search}`)
+  .then(res => res.json())
+  .then(body => {
+    if (body === null) return message.channel.send("Not found :(")
+    message.channel.send({ embed: body });
+  })
+
+ }
+}
