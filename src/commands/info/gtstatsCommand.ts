35bf80dee2a0f3059a44cde1331fb2d7c206bbf6
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-07 06:46:42 +0700

    Create gtstatsCommand.ts

---
+++ src/commands/info/gtstatsCommand.ts
@@ -0,0 +1,23 @@
+import { MessageEmbed } from "discord.js";
+const axios = require("axios");
+
+export default {
+  name: "gtstats",
+  aliases: ["statsgt", "gt-stats", "growtopia-stats"],
+  description: "stats about growtopia",
+  cooldown: 1,
+  guildOnly:true,
+  async execute(msg, args, client) {
+
+  let baseURL = "https://www.growtopiagame.com/detail";
+  let res = await axios.get(baseURL);
+
+  let embed = new MessageEmbed()
+  .setTitle("Growtopia Statistic")
+  .addField("Online User's", res.data.online_user + " users")
+  .addField("World Of The Day", res.data.world_day_images.fullsize.replace(/https:\/\/www.growtopiagame.com\/worlds\//g, "").replace(/.png/g, "")
+  .setImage(res.data.world_day_images.fullsize)
+  .setColor(client.color)
+  return msg.channel.send(embed)
+ }
+}
