2e88068a2222338cca70ed68ce00a1615aa6f326
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-07 06:49:46 +0700

    Update gtstatsCommand.ts

--- src/commands/info/gtstatsCommand.ts
+++ src/commands/info/gtstatsCommand.ts
@@ -17,7 +17,7 @@ export default {
   .addField("Online User's", res.data.online_user + " users")
   .addField("World Of The Day", res.data.world_day_images.fullsize.replace(/https:\/\/www.growtopiagame.com\/worlds\//g, "").replace(/.png/g, "")
   .setImage(res.data.world_day_images.fullsize)
-  .setColor(client.color)
+  .setColor(client.color);
   return msg.channel.send(embed)
  }
 }
