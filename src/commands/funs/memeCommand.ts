0c0ba820ae12b6199186be8936a96cf3ac9755b9
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-06 17:55:51 +0700

    Update memeCommand.ts

--- src/commands/funs/memeCommand.ts
+++ src/commands/funs/memeCommand.ts
@@ -6,7 +6,7 @@ export default {
   aliases: ["memes"],
   description: "maybe meme?",
   cooldown: 1,
-  async execute: (msg, args, client) => {
+  async execute(msg, args, client) => {
 
   const subReddits = ["dankmeme", "meme", "me_irl"];
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
