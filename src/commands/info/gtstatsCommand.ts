6d65a92968d236347c49fea3439459564a4fe6e0
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-07 06:53:55 +0700

    Update gtstatsCommand.ts

--- src/commands/info/gtstatsCommand.ts
+++ src/commands/info/gtstatsCommand.ts
@@ -15,7 +15,7 @@ export default {
   let embed = new MessageEmbed()
   .setTitle("Growtopia Statistic")
   .addField("Online User's", res.data.online_user + " users")
-  .addField("World Of The Day", res.data.world_day_images.fullsize.replace(/https:\/\/www.growtopiagame.com\/worlds\//g, "").replace(/.png/g, ""))
+  .addField("World Of The Day", res.data.world_day_images.fullsize.replace("https:\/\/www.growtopiagame.com\/worlds\/", "").replace(".png", ""))
   .setImage(res.data.world_day_images.fullsize)
   .setColor(client.color);
   return msg.channel.send(embed)
