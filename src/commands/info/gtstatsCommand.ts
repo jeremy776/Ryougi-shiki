9d4d98c3f7aced4d42ad39bb89ed78162ef0fcba
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-07 06:55:48 +0700

    Update gtstatsCommand.ts

--- src/commands/info/gtstatsCommand.ts
+++ src/commands/info/gtstatsCommand.ts
@@ -11,11 +11,12 @@ export default {
 
   let baseURL = "https://www.growtopiagame.com/detail";
   let res = await axios.get(baseURL);
+  let wotd = res.data.world_day_images.fullsize;
 
   let embed = new MessageEmbed()
   .setTitle("Growtopia Statistic")
   .addField("Online User's", res.data.online_user + " users")
-  .addField("World Of The Day", res.data.world_day_images.fullsize.replace("https:\/\/www.growtopiagame.com\/worlds\/", "").replace(".png", ""))
+  .addField("World Of The Day", wotd.replace("https:\/\/www.growtopiagame.com\/worlds\/", "").replace(".png", ""))
   .setImage(res.data.world_day_images.fullsize)
   .setColor(client.color);
   return msg.channel.send(embed)
