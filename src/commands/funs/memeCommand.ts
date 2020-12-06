79673558b92ee518f20133bd21f2d494af77c784
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-06 17:47:50 +0700

    Create memeCommand.ts

---
+++ src/commands/funs/memeCommand.ts
@@ -0,0 +1,21 @@
+import { MessageEmbed } from "discord.js";
+const randomPuppy = require("random-puppy");
+
+export default {
+  name: "meme",
+  aliases: ["memes"],
+  description: "maybe meme?",
+  cooldown: 1,
+  async execute: (msg, args, client) => {
+
+  const subReddits = ["dankmeme", "meme", "me_irl"];
+    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+    const img = await randomPuppy(random);
+    const embed = new MessageEmbed()
+    .setImage(img)
+    .setColor(c.COLOR)
+    .setURL(`https://reddit.com/r/${random}`)
+    .setTimestamp();
+    return msg.channel.send(embed)
+ }
+}
