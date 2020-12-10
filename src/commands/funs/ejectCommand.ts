4fcb5a90f6b87d9f5fa2412b5f5a5f12d1c45d30
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-11 06:49:08 +0700

    Create ejectCommand.ts

---
+++ src/commands/funs/ejectCommand.ts
@@ -0,0 +1,33 @@
+import { MessageEmbed } from "discord.js";
+
+export default {
+  name: "eject",
+  cooldown: 2,
+  descritpion: "who is the impostor?",
+  guildOnly:true,
+  execute(msg, args, client) {
+
+  let user = msg.mentions.users.first()
+  let noUser = MessageEmbed()
+  .setDescription("**User not found**")
+  .setColor(client.color)
+  if(!user) return msg.channel.send(noUser);
+
+  let isImpostors = "true false".repeat(100);
+  let impostor = isImpostors.split(" ");
+
+  let random = client.util.randomNumber(1, impostor.length);
+  let a = impostor[random];
+
+  let color = ["black", "blue", "brown", "cyan", "darkgreen", "lime", "orange", "pink", "purple", "red", "white", "yellow"];
+  let randomColor = client.randomNumber(1, color.length)
+  let colorRandom = color[randomColor];
+
+  let embed = new MessageEmbed()
+  .setImage(`https://vacefron.nl/api/ejected?name=${user.username}&impostor=${a}&crewmate=${colorRandom}`)
+  .setColor(client.color)
+  .setTimestamp()
+  .setFooter(`${msg.author.username} has voted ${user.username}`)
+  return msg.channel.send(embed)
+ }
+}
