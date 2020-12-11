fabfefc29ed195caef60902b3bf53cbf828b3821
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-12 02:15:28 +0700

    Update lockCommand.ts

--- src/commands/moderation/lockCommand.ts
+++ src/commands/moderation/lockCommand.ts
@@ -13,14 +13,14 @@ import type { Message } from "discord.js";
  export default class lockCommand extends Command {
    public async exec(message:Message, args:string[]) {
 
-  if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send({embed:{description: "**You do not have permission \`MANAGE_CHANNELS\` to lock this channel**", color:client.color}});
-  if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send({embed:{description: "**Give me \`MANAGE_CHANNELS\` permission if you want to run this command**", color:client.color}});
+  if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send({embed:{description: "**You do not have permission \`MANAGE_CHANNELS\` to lock this channel**", color:this.client.color}});
+  if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send({embed:{description: "**Give me \`MANAGE_CHANNELS\` permission if you want to run this command**", color:this.client.color}});
   message.channel.overwritePermissions([{
         id: message.guild.id,
         deny : ['SEND_MESSAGES'],
      },
     ],
    );
-  return message.channel.send({embed:{description:`**\`${message.author.tag}\` has locked this channel**`, color:client.color}});
+  return message.channel.send({embed:{description:`**\`${message.author.tag}\` has locked this channel**`, color:this.client.color}});
  }
 }
