4b52d68b20f0adb78331b8194b314b304e7f1c09
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-11 12:53:48 +0700

    Update uptimeCommand.ts

--- src/commands/info/uptimeCommand.ts
+++ src/commands/info/uptimeCommand.ts
@@ -1,16 +1,23 @@
 import { MessageEmbed } from "discord.js";
+import type { Message } from "discord.js";
+import { CommandConf } from "../../decorators";
+import Command from "../../handle/Command";
 
-export default {
+@CommandConf({
    name: "uptime",
-   cooldown:1,
    description: "how long has this bot been active?",
-   execute(msg, args, client) {
+   usage: "uptime",
+   cooldown: 1,
+   ownerOnly:false
+})
+
+ export default class uptimeCommand extends Command {
+   public async exec(msg:Message, args:[]) {
 
    let embed = new MessageEmbed()
-   .setDescription(`**${client.util.parseDur(client.uptime)}**`)
+   .setDescription(`**${this.client.util.parseDur(this.client.uptime)}**`)
    .setColor(client.color)
    .setTimestamp()
    return msg.channel.send(embed)
-
  }
 }
