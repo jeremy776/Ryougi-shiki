e6a4d77a29e508ba669b78e58c30bc500f98e2bc
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-12 00:09:13 +0700

    Update lockCommand.ts

--- src/commands/moderation/lockCommand.ts
+++ src/commands/moderation/lockCommand.ts
@@ -1,14 +1,20 @@
-export default {
+import { CommandConf } from "../../decorators";
+import Command from "../../handle/Command";
+import type { Message } from "discord.js";
+
+@CommandConf({
   name: "lock",
   aliases: ["lockdown"],
   cooldown: 5,
-  guildOnly: true,
-  description: "Lock channel",
-  execute(message, args, client) {
+  ownerOnly:false,
+  description: "Lock channel"
+})
+
+ export default class lockCommand extends Command {
+   public async exec(message:Message, args:[]) {
 
   if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send({embed:{description: "**You do not have permission \`MANAGE_CHANNELS\` to lock this channel**", color:client.color}});
   if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send({embed:{description: "**Give me \`MANAGE_CHANNELS\` permission if you want to run this command**", color:client.color}});
-   
   message.channel.overwritePermissions([{
         id: message.guild.id,
         deny : ['SEND_MESSAGES'],
