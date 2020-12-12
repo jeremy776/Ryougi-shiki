ca6b0a450752be69caac2d9ac55edae8a41588ab
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-12 20:01:09 +0700

    Update suggestionsCommand.ts

--- src/commands/util/suggestionsCommand.ts
+++ src/commands/util/suggestionsCommand.ts
@@ -1,22 +1,30 @@
 import { MessageEmbed } from "discord.js";
+import type { Message } from "discord.js";
+import { CommandConf } from "../../decorators";
+import Command from "../../handle/Command";
 
-export default {
+@CommandConf({
   name: "suggestions",
   aliases: ["suggestion", "suggest"],
   cooldown:60,
   description: "give me your suggestion for next update!",
-  execute(msg, args, client) {
+  ownerOnly:false,
+  usage: "suggestion [message]"
+})
+
+ export default class suggestionsCommand extends Command {
+   public async exec(msg:Message, args:string[]) {
 
   let suggest = args.join(" ");
   let noMsg = new MessageEmbed()
-  .setColor(client.color)
+  .setColor(this.client.color)
   .setDescription(`**${this.description}**`)
   if(!suggest) return msg.channel.send(noMsg)
 
   let embed = new MessageEmbed()
   .setAuthor("New Suggestion", msg.guild.iconURL({dynamic:true}))
   .setThumbnail(msg.author.displayAvatarURL({dynamic:true}))
-  .setColor(client.color)
+  .setColor(this.client.color)
   .addField("Guild Info", `
 \`\`\`
 • Name  :: ${msg.guild.name}
@@ -28,7 +36,7 @@ export default {
   - Id    :: ${msg.author.id}
 • Message :: ${suggest}
 \`\`\``)
-   msg.channel.send({embed:{description:"**Thanks For Give Suggestion:)**", color: client.color}})
-   return client.channels.cache.get(client.config.suggestion).send(embed)
+   msg.channel.send({embed:{description:"**Thanks For Give Suggestion:)**", color: this.client.color}})
+   return this.client.channels.cache.get(client.config.suggestion).send(embed)
  }
 }
