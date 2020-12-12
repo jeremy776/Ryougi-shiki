a2e6f6e33aefa50682ebf07c0df8eb24df37a50e
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-12 18:17:00 +0700

    Update randomNumberCommand.ts

--- src/commands/util/randomNumberCommand.ts
+++ src/commands/util/randomNumberCommand.ts
@@ -1,14 +1,21 @@
-export default {
+import { CommandConf } from "../../decorators";
+import type { Message } from "discord.js";
+import Command from "../../handle/Command";
+
+@CommandConf({
   name: "randomnumber",
   aliases: ["random-number", "number-generator"],
   description: "Get A Random Number\n• randomnumber [max]\n•default value: 50",
-  execute(msg, args, client) {
+  usage: "randomnumber[max number (default is 50)",
+  ownerOnly:false
+})
+
+ export default class randomNumber extends Command {
+   public async exec(msg:Message, args:string[]) {
 
   let max = args[0];
   if(!max) max = 50;
-
   if(isNaN(max)) return msg.reply("**Max Must Be A Number**")
-
-  return msg.reply("I got the number\n"+client.util.randomNumber(1, max))
+  return msg.reply("I got the number\n"+this.client.util.randomNumber(1, max))
  }
 }
