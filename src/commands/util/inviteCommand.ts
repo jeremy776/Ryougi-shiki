ed9f9889a33bd059add275d0e29d0f497be97333
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-12 18:03:30 +0700

    Update inviteCommand.ts

--- src/commands/util/inviteCommand.ts
+++ src/commands/util/inviteCommand.ts
@@ -1,16 +1,24 @@
 import { MessageEmbed } from "discord.js";
+import { CommandConf } from "../../decorators";
+import type { Message } from "discord.js";
+import Command from "../../handle/Command";
 
-export default {
+@CommandConf({
   name: "invite",
   cooldown:1,
   description: "invite me pls :)",
-  execute(msg, args, client) {
+  usage: "",
+  ownerOnly:false
+})
+
+ export default class inviteCommand extends Command {
+   public async exec(msg:Message, args:string[]) {
 
   let embed = new MessageEmbed()
   .setDescription("**Thank you for using our bot, and below is a link to invite me:)**")
-  .setColor(client.color)
+  .setColor(this.client.color)
   .setTimestamp()
-  .addField("🗒️ | Invite", `[Click Here](https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`)
+  .addField("🗒️ | Invite", `[Click Here](https://discordapp.com/api/oauth2/authorize?client_id=${this.client.user.id}&permissions=8&scope=bot)`)
   return msg.channel.send(embed);
  }
 }
