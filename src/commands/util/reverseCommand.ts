e03e456b6e22a93299a7c917c555d2fcb9049d67
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-12 20:33:38 +0700

    Update reverseCommand.ts

--- src/commands/util/reverseCommand.ts
+++ src/commands/util/reverseCommand.ts
@@ -13,7 +13,7 @@ import Command from "../../handle/Command";
 })
 
  export default class reverseCommand extends Command {
-   public async exec(msg:Message, args:string[]) {
+   public async exec(msg:Message, args:string[] || null) {
    try {
     args = args.join(" ") || "None";
     let reversed = "";
