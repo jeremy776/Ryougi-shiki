bc3ef357bb388809c9b5c3e89130c54fd512d26b
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-12 18:13:58 +0700

    Update pingCommand.ts

--- src/commands/util/pingCommand.ts
+++ src/commands/util/pingCommand.ts
@@ -1,15 +1,23 @@
 import { MessageEmbed } from 'discord.js';
+import { CommandConf } from "../../decorators";
+import type { Message } from "discord.js";
+import Command from "../../handle/Command";
 
-export default {
+@CommandConf({
   name: "ping",
   aliases: [],
   cooldown: 3,
-  guildOnly: false,
+  ownerOnly:false,
   description: "show bot ping",
-  execute(message, args, client) {
+  usage: ""
+})
+
+ export default class pingCommand extends Command {
+   public async exec(message:Message, args:string[]) {
+
     const embed = new MessageEmbed()
-        .addField("Pong!",  Math.round(client.ws.ping) + "ms", true)
-        .setColor(client.color)
+        .addField("Pong!",  Math.round(this.client.ws.ping) + "ms", true)
+        .setColor(this.client.color)
         .setTimestamp()
      message.channel.send(embed)
   }
