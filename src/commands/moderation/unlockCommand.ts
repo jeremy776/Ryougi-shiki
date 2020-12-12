add69f3bbf4c73acb3ae7b34c3f9f9e1d8147f3a
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-12 23:30:37 +0700

    Update unlockCommand.ts

--- src/commands/moderation/unlockCommand.ts
+++ src/commands/moderation/unlockCommand.ts
@@ -18,13 +18,13 @@ import Command from "../../handle/Command";
    let noPerms = new MessageEmbed()
    .setColor(this.client.color)
    .setDescription(`**You must have the \`MANAGE_CHANNELS\` permission**`);
-   if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send(noPerms);
+   if(!message.member?.hasPermission("MANAGE_CHANNELS")) return message.channel.send(noPerms);
    let clientNoPerms = new MessageEmbed()
    .setColor(this.client.color)
    .setDescription(`**I need to have the \`MANAGE_CHANNELS\` permission**`);
-   if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send(clientNoPerms);
+   if(!message.guild?.me?.hasPermission("MANAGE_CHANNELS")) return message.channel.send(clientNoPerms);
    message.channel.overwritePermissions([{
-        id: message.guild.id,
+        id: message.guild?.id,
         null: ['SEND_MESSAGES'],
      },
     ],
@@ -32,7 +32,7 @@ import Command from "../../handle/Command";
 
    let Succes = new MessageEmbed()
    .setColor(this.client.color)
-   .setDescription(`**\`${message.author.tag}\` has unlocked the channel**`)
+   .setDescription(`**\`${message.author?.tag}\` has unlocked the channel**`)
    return message.channel.send(Succes)
  }
 }
