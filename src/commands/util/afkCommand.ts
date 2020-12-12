b80015f5f74b3f30a4f88cb11728d40a45add3c5
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-12 17:52:43 +0700

    Update afkCommand.ts

--- src/commands/util/afkCommand.ts
+++ src/commands/util/afkCommand.ts
@@ -14,19 +14,19 @@ import type { Message } from "discord.js";
  export default class afkCommand extends Command {
    public async exec(msg:Message, args:string[]) {
     
-    const afk = client.afk.get(msg.author.id)
+    const afk = this.client.afk.get(msg.author.id)
 
     const embedAFK = new MessageEmbed()
-    .setColor(client.color)
+    .setColor(this.client.color)
     let k = args.join(" ")
     if(!k) k = "Not Set";
     
     if (!afk) {
       embedAFK.setDescription(`**${msg.author.tag}** now afk with reason: \`${k}\``)
-      client.afk.set(msg.author.id, k as any);
+      this.client.afk.set(msg.author.id, k as any);
     } else {
       embedAFK.setDescription("**You're no longer Afk**");
-      client.afk.delete(msg.author.id);
+      this.client.afk.delete(msg.author.id);
     }
     
     return msg.channel.send(embedAFK)
