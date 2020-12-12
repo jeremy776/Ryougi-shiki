fff3acfb691be40d8b27b5b77620d347d048e4fb
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-12 17:59:37 +0700

    Update howgayCommand.ts

--- src/commands/util/howgayCommand.ts
+++ src/commands/util/howgayCommand.ts
@@ -1,18 +1,25 @@
 import { MessageEmbed } from "discord.js";
+import { CommandConf } from "../../decorators";
+import type { Message } from "discord.js";
+import Command from "../../handle/Command";
 
-export default {
+@CommandConf({
   name: "howgay",
   description: "you gay!",
   cooldown: 3,
-  guildOnly:true,
-  execute(msg, args, client) {
+  usage: "howgay @user",
+  ownerOnly:false
+})
+
+ export default class howgayCommand extends Command {
+   public async exec(msg:Message, args:string[]) {
 
   let user = msg.mentions.users.first() || msg.author;
   if(user.id == client.user.id) return msg.reply("I'll never be gay!")
   let embed = new MessageEmbed()
-  .setColor(client.color)
+  .setColor(this.client.color)
   .setAuthor(user.tag + " is gay?", user.displayAvatarURL({dynamic:true}))
-  .setDescription(client.util.randomNumber(1, 100)+"%")
+  .setDescription(thus.client.util.randomNumber(1, 100)+"%")
   .setTimestamp()
   return msg.channel.send(embed)
 
