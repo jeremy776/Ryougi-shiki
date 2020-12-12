fcce3369dd10fb46a481fd39699f2cfa40bc131c
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-12 18:22:43 +0700

    Update reportCommand.ts

--- src/commands/util/reportCommand.ts
+++ src/commands/util/reportCommand.ts
@@ -1,25 +1,32 @@
 import { MessageEmbed } from "discord.js";
+import { CommandConf } from "../../decorators";
+import type { Message } from "discord.js";
+import Command from "../../handle/Command";
 
-export default {
+@CommandConf({
   name: "report",
   aliases: ["bug"],
-  guildOnly: true,
+  ownerOnly:false,
   cooldown: 60,
   description: "if you find a bug, you can use the report to our staff",
-  execute(msg, args, client) {
+  usage: "report [message]"
+})
+
+ export default class reportCommand extends Command {
+   public async exec(msg:Message, args:string[]) {
 
   let pesan = args.join(" ")
   let invalidMsg = new MessageEmbed()
   .setDescription("**Enter any bugs you find in our bots**")
-  .setColor(client.color)
+  .setColor(this.client.color)
   .setTimestamp()
-  .setFooter(`ex: ${client.config.prefix}report [bug]`)
+  .setFooter(`ex: ${this.client.config.prefix}report [bug]`)
   if(!pesan) return msg.channel.send(invalidMsg)
 
   /*if(typeof pesan !== "string") {
     let notAstring = new MessageEmbed()
     .setDescription("**the message you enter must be a string**")
-    .setColor(client.color)
+    .setColor(this.client.color)
     .setTimestamp()
     return msg.channel.send(notAstring)
   }
@@ -27,21 +34,21 @@ export default {
   if(pesan.length < 5) {
     let shortMessage = new MessageEmbed()
     .setDescription("**Your message is too short**")
-    .setColor(client.color)
+    .setColor(this.client.color)
     .setTimestamp()
     return msg.channel.send(shortMessage)
   }*/
 
   let Succes = new MessageEmbed()
   .setDescription("**Your report has been successfully sent to our developer**")
-  .setColor(client.color)
+  .setColor(this.client.color)
   .setTimestamp()
   msg.channel.send(Succes)
 
   let reportChannels = new MessageEmbed()
   .setAuthor(`New Report by ${msg.author.tag}`, msg.guild.iconURL({dynamic:true}))
   .setThumbnail(msg.author.displayAvatarURL({dynamic:true}))
-  .setColor(client.color)
+  .setColor(this.client.color)
   .setTimestamp()
   .addField("Author", `\`\`\`
 Name          :: ${msg.author.username}
@@ -58,7 +65,7 @@ Bug Report :: ${pesan}
 
     if(msg.attachments.first()) reportChannels.setImage(msg.attachments.first().proxyURL);
 
-   return client.channels.cache.get(client.config.reportChannel).send(reportChannels)
+   return this.client.channels.cache.get(client.config.reportChannel).send(reportChannels)
 
  }
 }
