7bad633045c8f79e6f44229fef9af5862d161073
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-13 20:08:04 +0700

    Create User.ts

---
+++ src/extenders/User.ts
@@ -0,0 +1,15 @@
+import { Structures } from "discord.js";
+
+class User extends Structures.get("User") {
+    public get isDev(): boolean {
+        return this.client.config.owners.includes(this.id);
+    }
+}
+
+declare module "discord.js" {
+    export interface User {
+        isDev: boolean;
+    }
+}
+
+Structures.extend("User", () => User);
