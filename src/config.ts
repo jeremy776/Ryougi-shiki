5faedbcbb21a2dd1c1146235457237be019548fb
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-13 16:15:09 +0700

    Update and rename config.json to config.ts

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export default {
+  prefix: "r.",
+  owner: ["580640622235484161"],
+  reportChannel: "784761343261278258",
+  suggestion: "784748900212408321",
+  mongoURL: "mongodb+srv://jeremy:jeremy@cluster0.d0mjj.mongodb.net/jeremy?retryWrites=true&w=majority",
+  token: "NjU0OTkxODczODYxMjIyNDMx.XfNmtw.UyQio8ZPyqKGtLNFmN0QwijDQVs"
+}
