da59a1182363dcac5ef4487ddb4114e7395eeb65
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-13 15:55:07 +0700

    Update messageDeleteEvent.ts

--- src/listeners/messageDeleteEvent.ts
+++ src/listeners/messageDeleteEvent.ts
@@ -2,7 +2,7 @@ import type { Message, TextChannel } from "discord.js";
 import Listener from "../handle/Listener";
 
 export default class MessageDeleteEvent extends Listener {
-  public name: "messageDelete"
+  public name = "messageDelete"
   public async exec(msg:Message) {
     let channel = msg.channel as TextChannel;
     this.client.snipe.set(channel.id, msg)
