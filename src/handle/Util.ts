77a97a6f2267f1f6d12c309fe9ab35597a80e896
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-13 20:36:39 +0700

    Update Util.ts

--- src/handle/Util.ts
+++ src/handle/Util.ts
@@ -1,6 +1,6 @@
 const fetch = require("node-superfetch");
 import { Client } from "discord.js";
-export default class Util {
+export default class Utility {
    public constructor(public readonly client : Client){}
    public cpu() {
   let cpuUsage = process.cpuUsage();
