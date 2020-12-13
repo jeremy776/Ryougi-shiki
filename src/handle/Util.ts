bfac815772970f92a395ff32b1126886a9bd4157
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-13 10:58:59 +0700

    Update Util.ts

--- src/handle/Util.ts
+++ src/handle/Util.ts
@@ -1,13 +1,15 @@
 const fetch = require("node-superfetch");
 
-export class Util {
+export default class Util {
 
- static cpu() {
+ public constructor(public readonly client : Client){}
+
+ public cpu() {
   let cpuUsage = process.cpuUsage();
   return (cpuUsage.user/cpuUsage.system).toFixed(2) + "%";
  }
 
- static parseDur(ms: number) {
+ public parseDur(ms: number) {
    let S = ms / 1000;
  
   let D = parseInt((S / 86400).toString());
@@ -22,12 +24,12 @@ export class Util {
   if (S || M || H || D) return `${D}d ${H}h ${M}m ${S}s`;
  }
  
- static async hastebin(string: string) {
+ public async hastebin(string: string) {
    const { body } = await fetch.post('https://hasteb.in/documents').send(string);
    return `https://hasteb.in/${body.key}.js`
  }
 
- static randomNumber(min: number, max: number):number {
+ public randomNumber(min: number, max: number):number {
    if(!min) throw new Error("Invalid Min Number")
    if(!max) throw new Error("Invalid Max Number")
    if(isNaN(min)) throw new Error("Value Must be A Number")
