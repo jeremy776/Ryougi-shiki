30a6b0bff1f5c7a6a9ee5535ee5cf9ceb3264e60
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-13 11:07:05 +0700

    Update Util.ts

--- src/handle/Util.ts
+++ src/handle/Util.ts
@@ -1,13 +1,13 @@
 const fetch = require("node-superfetch");
 import { Client } from "discord.js";
-export default class Util {
+export class Util {
 
- public constructor(public readonly client : Client){}
- public cpu() {
+ //static constructor(public readonly client : Client){}
+ static cpu() {
   let cpuUsage = process.cpuUsage();
   return (cpuUsage.user/cpuUsage.system).toFixed(2) + "%";
  }
- public parseDur(ms: number) {
+ static parseDur(ms: number) {
    let S = ms / 1000;
  
   let D = parseInt((S / 86400).toString());
@@ -21,11 +21,11 @@ export default class Util {
  
   if (S || M || H || D) return `${D}d ${H}h ${M}m ${S}s`;
  }
- public async hastebin(string: string) {
+ static async hastebin(string: string) {
    const { body } = await fetch.post('https://hasteb.in/documents').send(string);
    return `https://hasteb.in/${body.key}.js`
  }
- public randomNumber(min: number, max: number):number {
+ static randomNumber(min: number, max: number):number {
    if(!min) throw new Error("Invalid Min Number")
    if(!max) throw new Error("Invalid Max Number")
    if(isNaN(min)) throw new Error("Value Must be A Number")
