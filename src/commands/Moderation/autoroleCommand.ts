20203044ecef98d0be9a24851329ba4484f30dbc
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-14 16:22:49 +0700

    Update autoroleCommand.ts

--- src/commands/Moderation/autoroleCommand.ts
+++ src/commands/Moderation/autoroleCommand.ts
@@ -25,22 +25,6 @@ import Command from "../../handle/Command";
 
    if(!["on", "off", "set"].includes(args[0])) return msg.reply(`Invalid options, please use \`${this.client.config.prefix}help autorole\` for info`)
 
-   if(args[0].toLowerCase() == "off") {
-     let status = this.client.db.get(`autorole.${msg.guild?.id}`)
-     //if(status == undefined) return msg.reply("**You must set a role first**");
-     if(status.status == false) return msg.reply("**autorole is not active**");
-     if(status.status == true) {
-         let off = new MessageEmbed()
-         .setDescription("**you have disabled the autorole command**")
-         .setColor(this.client.color)
-         this.client.db.set(`autorole.${msg.guild?.id}`, {
-          id:status.id,
-          status:false
-         });
-         return msg.channel.send(off)
-     }
-   }
-
    if(args[0].toLowerCase() == "set") {
       let data = this.client.db.get(`autorole.${msg.guild?.id}`);
       let status = data.status;
@@ -62,6 +46,20 @@ import Command from "../../handle/Command";
         })
        return msg.channel.send(succes)
     }
+
+    if(args[0].toLowerCase() == "on") {
+     let status = this.client.db.get(`autorole.${msg.guild?.id}`)
+     if(status == undefined) return msg.reply("**You must set a role first**");
+     if(status.status == true) return msg.reply("**autorole is already active**");
+      let on = new MessageEmbed()
+      .setDescription("**you have enable the autorole**")
+      .setColor(this.client.color)
+      this.client.db.set(`autorole.${msg.guild?.id}`, {
+        id:status.id,
+        status:true
+       });
+      return msg.channel.send(on)
+   }
 
  }
 }
