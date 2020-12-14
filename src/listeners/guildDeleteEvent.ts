c91cd3e03bddf7b1a99e36b476c31a4086a77752
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-14 22:05:33 +0700

    Update guildDeleteEvent.ts

--- src/listeners/guildDeleteEvent.ts
+++ src/listeners/guildDeleteEvent.ts
@@ -6,5 +6,13 @@ export default class GuildDeleteEvent extends Listener {
   public name = "guildDelete";
   public async exec(guild:Guild) {
 
+  this.client.db.delete(guild.id)
+  let embed = new MessageEmbed()
+  .setAuthor("👋 LEAVE GUILD", guild.iconURL({dynamic:true}) as any)
+  .addField("📝 NAME", guild.name)
+  .addField("🔬 OWNER", guild.owner.user?.tag)
+  .addField("⚙️ MEMBER", guild.memberCount)
+  .setColor(this.client.color)
+  return this.client.channels.cache.get(this.client.config.logs).send(embed)
  }
 }
