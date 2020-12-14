e68c497a3aef2e6d8ff30bd6c62964871f72ba74
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-14 22:08:32 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ export default {
   owner: ["580640622235484161"],
   reportChannel: "784761343261278258",
   suggestion: "784748900212408321",
+  logs: "788059738680066068",
   mongoURL: "mongodb+srv://jeremy:jeremy@cluster0.d0mjj.mongodb.net/jeremy?retryWrites=true&w=majority",
   token: "NjU0OTkxODczODYxMjIyNDMx.XfNmtw.UyQio8ZPyqKGtLNFmN0QwijDQVs"
 }
