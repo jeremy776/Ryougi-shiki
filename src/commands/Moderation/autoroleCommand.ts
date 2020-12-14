d7b6ca04c895d0d7db0f7927e3e9d122791db19f
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-14 16:18:09 +0700

    Update autoroleCommand.ts

--- src/commands/Moderation/autoroleCommand.ts
+++ src/commands/Moderation/autoroleCommand.ts
@@ -19,7 +19,7 @@ import Command from "../../handle/Command";
   .setDescription("**You must have \`MANAGE SERVER\` permission to use this command**")
   .setColor(this.client.color)
 
-  if(!msg.member?.hasPermission("MANAGE_SERVER")) return msg.channel.send(noPerms);
+  if(!msg.member?.hasPermission("MANAGE_GUILD")) return msg.channel.send(noPerms);
   if(!msg.guild?.me?.hasPermission("MANAGE_ROLES")) return msg.channel.send({embed:{description: "**Give me \`MANAGE_ROLES\` permission if you want to run this command**", color:this.client.color}});
 
 
