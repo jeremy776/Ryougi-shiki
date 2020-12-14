fc5d2cd88e1e7b242dcdb54f9e8fabe25da2d903
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-14 16:01:48 +0700

    Update autoroleCommand.ts

--- src/commands/Moderation/autoroleCommand.ts
+++ src/commands/Moderation/autoroleCommand.ts
@@ -7,7 +7,7 @@ import Command from "../../handle/Command";
   name: "autorole",
   aliases: ["auto-role"],
   description: "assign roles to members who have just come to the server",
-  usage: "autorole set [@role || name role]\nautorole [on || off]",
+  usage: "autorole set [@role or name role]\nautorole [on or off]",
   cooldown: 1,
   ownerOnly:false
 })
@@ -16,44 +16,38 @@ import Command from "../../handle/Command";
    public async exec(msg:Message, args:string[]) {
 
    if(!["on", "off", "set"].includes(args[0])) return msg.reply(`Invalid options, please use \`${this.client.config.prefix}help autorole\` for info`)
-
    if(args[0].toLowerCase() == "on") {
-     let status = this.client.db.get(`autorole.${msg.guild?.id}`)
-     if(status == undefined) return msg.reply("**You must set a role first**");
-     if(status.status == true) return msg.reply("**autorole is already active**");
-     if(status.status == false) {
+     let data = this.client.db.get(`autorole.${msg.guild?.id}`)
+     let status = data.status;
+     //if(status == undefined) return msg.reply("**You must set a role first**");
+     if(status == true) return msg.reply("**autorole is already active**");
+     if(status == false) {
          let on = new MessageEmbed()
          .setDescription("**You have turned on autorole**")
          .setColor(this.client.color)
-
          this.client.db.set(`autorole.${msg.guild?.id}`, {
           id:status.id,
           status:true
          });
-
          return msg.channel.send(on)
      }
    }
 
-
    if(args[0].toLowerCase() == "off") {
      let status = this.client.db.get(`autorole.${msg.guild?.id}`)
-     if(status == undefined) return msg.reply("**You must set a role first**");
+     //if(status == undefined) return msg.reply("**You must set a role first**");
      if(status.status == false) return msg.reply("**autorole is not active**");
      if(status.status == true) {
          let off = new MessageEmbed()
          .setDescription("**you have disabled the autorole command**")
          .setColor(this.client.color)
-
          this.client.db.set(`autorole.${msg.guild?.id}`, {
           id:status.id,
           status:false
          });
-
          return msg.channel.send(off)
      }
    }
-
 
    if(args[0].toLowerCase() == "set") {
       let data = this.client.db.get(`autorole.${msg.guild?.id}`);
