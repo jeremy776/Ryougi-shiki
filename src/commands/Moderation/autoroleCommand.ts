d7df9f88e7086f4ea58f504f7676b0d00de9fea0
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-14 16:07:19 +0700

    Update autoroleCommand.ts

--- src/commands/Moderation/autoroleCommand.ts
+++ src/commands/Moderation/autoroleCommand.ts
@@ -15,6 +15,14 @@ import Command from "../../handle/Command";
  export default class autoroleCommand extends Command {
    public async exec(msg:Message, args:string[]) {
 
+  let noPerms = new MessageEmbed()
+  .setDescription("**You must have \`MANAGE SERVERS\` permission to use this command**")
+  .setColor(this.client.color)
+
+  if(!msg.member?.hasPermission("MANAGE_SERVERS")) return msg.channel.send(noPerms);
+  if(!msg.guild?.me?.hasPermission("MANAGE_ROLES")) return msg.channel.send({embed:{description: "**Give me \`MANAGE_ROLES\` permission if you want to run this command**", color:this.client.color}});
+
+
    if(!["on", "off", "set"].includes(args[0])) return msg.reply(`Invalid options, please use \`${this.client.config.prefix}help autorole\` for info`)
    if(args[0].toLowerCase() == "on") {
      let data = this.client.db.get(`autorole.${msg.guild?.id}`)
