59cde559576d78c720cfec2fd63c6977d97296f4
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-14 15:42:20 +0700

    Update and rename guildDelete.ts to guildDeleteEvent.ts

--- src/listeners/guildDeleteEvent.ts
+++ src/listeners/guildDeleteEvent.ts
@@ -1,8 +1,10 @@
-import type { Message } from "discord.js";
+import type { Guild } from "discord.js";
 import { MessageEmbed } from "discord.js";
 import Listener from "../handle/Listener";
 
 export default class GuildDeleteEvent extends Listener {
   public name = "guildDelete";
+  public async exec(guild:Guild) {
 
+ }
 }
