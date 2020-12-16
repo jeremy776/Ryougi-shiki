09e59278920c31a7ac64ea41c8dac59992543ed1
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-16 16:00:24 +0700

    Update guildMemberAddEvent.ts

--- src/listeners/guildMemberAddEvent.ts
+++ src/listeners/guildMemberAddEvent.ts
@@ -5,6 +5,16 @@ export default class GuildMemberAdd extends Listener {
   public async exec(member: any) {
 
   let data = await this.client.db.get(`autorole.${member.guild?.id}`)
+  let welcome = await this.client.db.get(`welcome.${member.guild?.id}`)
+
+  if(!welcome) return;
+  if(welcome.status == false) return;
+  let pesan = welcome.message.replace(/{member}/g, member).replace(/{member.tag}/g, member.user.tag).replace(/{member.id}/g, member.user.id).replace(/{member.name}/g, member.user.username).replace(/{server.name}/g, member.guild.name).replace(/{memberCount}/g, member.guild.memberCount).replace(/{server.id}/g, member.guild.id)
+  if(welcome.status == true) {
+    let channel = member.guild?.channels.cache.get(welcome.id) as TextChannel
+    channel.send(pesan)
+  }
+
   if(!data) return;
   if(data.status == false) return;
   if(data.status == true) {
