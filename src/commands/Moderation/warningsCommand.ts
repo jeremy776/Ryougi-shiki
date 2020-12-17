5813654820d8f43e57fbac8ea63c97301b7d40e9
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-17 16:04:09 +0700

    Create warningsCommand.ts

---
+++ src/commands/Moderation/warningsCommand.ts
@@ -0,0 +1,29 @@
+import { MessageEmbed } from "discord.js";
+import type { Message } from "discord.js";
+import { CommandConf } from "../../decorators";
+import Command from "../../handle/Command";
+
+@CommandConf({
+  name: "warnings",
+  aliases: ["checkwarn"],
+  description: "check user warnings",
+  usage: "warnings [@user]",
+  cooldown: 1,
+  ownerOnly:false
+})
+
+ export default class warningsCommand extends Command {
+   public async exec(msg:Message, args:string[]) {
+
+    let user = msg.mentions.users.first() || msg.author
+    let data = await this.client.db.get(`warning.${msg.guild?.id}_${user.id}`)
+
+    let embed = new MessageEmbed()
+    .setAuthor("🔔 Check Warns", msg.author.displayAvatarURL({dynamic:true}) as any)
+    .setColor(this.client.color)
+    .setTimestamp()
+    .setDescription(`**${user.tag} have ${data.length} warn's**`)
+    .addField("**Last 10 Warns**", data.map((x:any) => `**${x.reason}** • ${require("moment")(x.timestamp).fromNow()}`))
+    return msg.channel.send(embed)
+ }
+}
