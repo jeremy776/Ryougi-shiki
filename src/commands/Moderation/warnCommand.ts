13d0dcbc6a49a2e85b3cffdd39c57fc3e3396921
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-17 14:45:10 +0700

    Update warnCommand.ts

--- src/commands/Moderation/warnCommand.ts
+++ src/commands/Moderation/warnCommand.ts
@@ -56,13 +56,6 @@ import Command from "../../handle/Command";
       timestamp:Date.now()
    }
 
-   let data = await this.client.db.get(`warning.${msg.guild?.id}.${user.id}`);
-   if (!data) {
-    this.client.db.set(`warning.${msg.guild?.id}.${user.id}`, []);
-    data = [];
-   }
-
-  data.push(userData);
-  return this.client.db.set(`warning.${msg.guild?.id}.${user.id}`, data);
+   return this.client.db.push(`warning.${msg.guild?.id}.${user.id}`, userData);
  }
 }
