aff510adeff1780437b5ea8c0b8ad758ed48ecde
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-18 15:27:49 +0700

    Update unmuteCommand.ts

--- src/commands/Moderation/unmuteCommand.ts
+++ src/commands/Moderation/unmuteCommand.ts
@@ -18,9 +18,9 @@ import Command from "../../handle/Command";
    let user = msg.mentions.members?.first()
    if(!user) return msg.reply("**You must mention members who have been mute**")
 
-   let check = msg.guild?.roles?.cache.filter((x:any) => x.name.toLowerCase() == "muted").first()
-   if(!user.roles?.cache.has(check?.id)) return msg.reply("**Maybe the role is not detected, or this user is not muted**")
-   user.roles?.remove(check?.id)
+   let check = msg.guild?.roles?.cache.filter((x:any) => x.name.toLowerCase() == "muted").first().id
+   if(!user.roles?.cache.has(check)) return msg.reply("**Maybe the role is not detected, or this user is not muted**")
+   user.roles?.remove(check)
 
    let embed = new MessageEmbed()
    .setAuthor("🔊 UNMUTE", msg.author.displayAvatarURL({dynamic:true}) as any)
