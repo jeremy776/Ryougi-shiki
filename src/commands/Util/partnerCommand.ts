c2f5419791d77dc5bf0d7c8a089a8a1072d49d6c
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-18 08:03:50 +0700

    Update partnerCommand.ts

--- src/commands/Util/partnerCommand.ts
+++ src/commands/Util/partnerCommand.ts
@@ -17,7 +17,7 @@ import Command from "../../handle/Command";
 
    let embed = new MessageEmbed()
    .setColor(this.client.color)
-   .setAuthor(this.client.user.username + " Partners", this.client.displayAvatarURL())
+   .setAuthor(this.client.user.username + " Partners", this.client.user.displayAvatarURL() as any)
    .addField("Partner's", `[${this.client.fetchInvite("https://discord.gg/pXzknDy7vv").then(x => x.guild.name)}](https://discord.gg/pXzknDy7vv)`)
    .setTimestamp()
    return msg.channel.send(embed)
