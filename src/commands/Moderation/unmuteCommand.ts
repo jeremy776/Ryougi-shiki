e8ce2377735e407be2c4308d4b9de1ef21fbd5ca
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-18 15:26:12 +0700

    Update unmuteCommand.ts

--- src/commands/Moderation/unmuteCommand.ts
+++ src/commands/Moderation/unmuteCommand.ts
@@ -20,6 +20,7 @@ import Command from "../../handle/Command";
 
    let check = msg.guild?.roles?.cache.filter((x:any) => x.name.toLowerCase() == "muted").first()
    if(!user.roles?.cache.has(check?.id)) return msg.reply("**Maybe the role is not detected, or this user is not muted**")
+   user.roles?.remove(check?.id)
 
    let embed = new MessageEmbed()
    .setAuthor("🔊 UNMUTE", msg.author.displayAvatarURL({dynamic:true}) as any)
@@ -29,7 +30,6 @@ import Command from "../../handle/Command";
    .addField("Time Stamp", `**\`${require("moment")(Date.now()).format("ddd MMM DD YYYY hh:mm:ss")}\`**`)
    .setFooter("Congratulations, you are no longer muted")
    .setTimestamp()
-   msg.channel.send(embed)
-   return user.roles?.remove(check?.id)
+   return msg.channel.send(embed)
  }
 }
