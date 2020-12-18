66b74added6f5467b36dd5fd46565ad8270a59eb
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-18 08:00:57 +0700

    Create partnerCommand.ts

---
+++ src/commands/Util/partnerCommand.ts
@@ -0,0 +1,26 @@
+import { MessageEmbed } from "discord.js";
+import type { Message } from "discord.js";
+import { CommandConf } from "../../decorators";
+import Command from "../../handle/Command";
+
+@CommandConf({
+  name: "partner",
+  aliases: [],
+  description: "who is my partner?",
+  usage: "partner",
+  cooldown: 3,
+  ownerOnly:false
+})
+
+ export default class PartnerCommand extends Command {
+   public async exec(msg:Message, args:string[]) {
+
+   let embed = new MessageEmbed()
+   .setColor(this.client.color)
+   .setAuthor(this.client.user.username + " Partners", this.client.displayAvatarURL())
+   .addField("Partner's", `[${this.client.fetchInvite("https://discord.gg/pXzknDy7vv").then(x => x.guild.name)}](https://discord.gg/pXzknDy7vv)`)
+   .setTimestamp()
+   return msg.channel.send(embed)
+
+ }
+}
