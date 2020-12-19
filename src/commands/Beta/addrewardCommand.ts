28b275456871129766cf14b4ec3a9f6e7a13f7a1
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-19 23:23:35 +0700

    Update and rename setrewardCommand.ts to addrewardCommand.ts

--- src/commands/Beta/addrewardCommand.ts
+++ src/commands/Beta/addrewardCommand.ts
@@ -4,9 +4,9 @@ import type { Message } from "discord.js";
 import Command from "../../handle/Command";
 
 @CommandConf({
-  name: "setreward",
-  aliases: [],
-  description: "Set reward when the member gets the level that has been set and will get a role",
+  name: "addreward",
+  aliases: ["addrewards"],
+  description: "Add reward when the member gets the level that has been set and will get a role",
   usage: "setreward <level> <roles>",
   cooldown: 3,
   ownerOnly:true
