93bf49fbd491978872418f1cc200edadb2dff060
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-21 19:25:14 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import RyougiClient from './handle/RyougiClient';
+import express from "express";
+const app = express();
 
 const Discord = require("discord.js");
 const client = new RyougiClient({disableMentions: 'everyone', fetchAllMembers:true});
 
+app.get("/", function(req, res) {
+  res.send("Hello World");
+})
+
+app.listen(process.env.PORT);
 client.run();
