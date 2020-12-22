b1c93cdab23b40c7e6925242a81e8866ed7e7fe5
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-22 20:39:28 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/image", function(req, res) {
-  res.sendFile("./image_forest.jpg");
+  res.send(imgURL: "https://www.w3schools.com/howto/img_forest.jpg");
 });
 
 app.listen(process.env.PORT);
