8eb79a38a96c8c24581a18e868786f0d6696aa8b
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2020-12-24 12:19:21 +0700

    Update guildMemberRemoveEvent.ts

--- src/listeners/guildMemberRemoveEvent.ts
+++ src/listeners/guildMemberRemoveEvent.ts
@@ -5,7 +5,17 @@ export default class GuildRemoveAdd extends Listener {
   public name = "guildRemoveAdd";
   public async exec(member: any) {
 
-  //this.client.db.delete(`warning${member.guild?.id}.${member.user?.id}`)
+  let leave = await this.client.db.get(`leave.${member.guild.id}`);
+  if(!leave) return;
+
+  if(member.guild.id == leave.serverId) {
+  if(leave.status == false) return;
+  let pesan = leave.message.replace(/{member}/g, member).replace(/{member.tag}/g, member.user.tag).replace(/{member.id}/g, member.user.id).replace(/{member.name}/g, member.user.username).replace(/{server.name}/g, member.guild.name).replace(/{memberCount}/g, member.guild.memberCount).replace(/{server.id}/g, member.guild.id)
+  if(leave.status == true) {
+    let channel = member.guild?.channels.cache.get(leave.id) as TextChannel
+    channel.send(pesan)
+  }
+ }
 
  }
 }
