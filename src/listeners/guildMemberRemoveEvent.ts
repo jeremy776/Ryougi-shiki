41f3fa95906987020d8b395caf0b6c3d5c05806e
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2021-01-13 17:37:35 +0700

    Update guildMemberRemoveEvent.ts

--- src/listeners/guildMemberRemoveEvent.ts
+++ src/listeners/guildMemberRemoveEvent.ts
@@ -14,7 +14,26 @@ export default class GuildRemoveAdd extends Listener {
   let pessan = pesan.replace(/{member}/g, member).replace(/{member.tag}/g, member.user.tag).replace(/{member.id}/g, member.user.id).replace(/{member.name}/g, member.user.username).replace(/{server.name}/g, member.guild.name).replace(/{memberCount}/g, member.guild.memberCount).replace(/{server.id}/g, member.guild.id)
   if(leave.status == true) {
     let channel = member.guild?.channels.cache.get(leave.id) as TextChannel
-    channel.send(pessan)
+    const canvas = require("discord-canvas"),
+       leaveCanvas = new canvas.Leave();
+
+      let image = await leaveCanvas
+        .setUsername(member.user.username)
+        .setDiscriminator(member.user.discriminator)
+        .setMemberCount(member.user.memberCount)
+        .setGuildName(member.user.name)
+        .setAvatar(member.user.displayAvatarURL({ format: "png", size: 2048 }))
+        .setColor("border", "white")
+        .setColor("username-box", "black")
+        .setColor("discriminator-box", "black")
+        .setColor("message-box", "black")
+        .setColor("title", "white")
+        .setColor("avatar", "white")
+        .setBackground(bg)
+        .toAttachment();
+
+        let attachment = new MessageAttachment(image.toBuffer(), "welcome-image.png");
+        channel.send(pesan, attachment);
   }
  }
 
