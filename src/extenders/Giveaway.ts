3642e650a0e7d18c4ea213b9378dcd12c2781725
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2021-01-18 23:20:20 +0700

    Update Giveaway.ts

--- src/extenders/Giveaway.ts
+++ src/extenders/Giveaway.ts
@@ -3,7 +3,27 @@ const { GiveawaysManager } = require('discord-giveaways');
 class Giveaway extends GiveawaysManager {
 
   async getAllGiveaways() {
-    
+    return await this.client.db.get("giveaways");
+  }
+
+  async editGiveaways(id, data) {
+    let giveaway = await this.client.db.get("giveaways");
+    let newData = giveaway.filter((g) => g.messageID !== id);
+    newData.push(data);
+    await this.client.db.set("giveaways", newData);
+    return true;
+  }
+
+  async saveGiveaway(id, data) {
+    await this.client.db.push("giveaways", data);
+    return true;
+  }
+
+  async deleteGiveaway(id) {
+    let data = await this.client.db.get("giveaways")
+    let newData = data.filter((d) => d.messageID !== id)
+    await this.client.db.set("giveaways", newData);
+    return true;
   }
 
 }
