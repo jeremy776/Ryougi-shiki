0c9539b30b995e33271e6341416e27f23911e116
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2021-01-28 22:28:15 +0700

    Update triggeredCommand.ts

--- src/commands/Image/triggeredCommand.ts
+++ src/commands/Image/triggeredCommand.ts
@@ -23,10 +23,10 @@ export default class TriggeredCommand extends Command {
   let img = await new this.client.image.Triggered().getImage(user.displayAvatarURL({ format: "png", size: 2048 }));
   let image = new MessageAttachment(img, "triggered.gif");
   let embed = new MessageEmbed()
-  .setImage("attachment://triggered.gif")
+  /*.setImage("attachment://triggered.gif")
   .setColor(this.client.color)
-  .setTimestamp()
-  return message.channel.send(embed);
+  .setTimestamp()*/
+  return message.channel.send(image);
 
  }
 }
