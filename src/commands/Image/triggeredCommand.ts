bf4e8667429a1c446ecc5dbc4be3e3282b2fb0b9
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2021-01-28 16:34:01 +0700

    Update triggeredCommand.ts

--- src/commands/Image/triggeredCommand.ts
+++ src/commands/Image/triggeredCommand.ts
@@ -1,6 +1,16 @@
 import { CommandConf } from "../handle/CommandConf";
 import Command from "../../decorators";
+import { MessageEmbed, MessageAttachment } from "discord.js";
+import type { Message } from "discord.js";
 
 @CommandConf({
-  name: "triggered",
-  
+ name: "triggered",
+ aliases: [],
+ description: "None",
+ usage: "triggered [ @user || user id ]",
+ cooldown:3,
+ ownerOnly:false
+})
+
+export default TriggeredCommand extends Command {
+  async exec(
