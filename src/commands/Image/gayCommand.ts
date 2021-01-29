edd7473e2dba224c4154ff8b35fdb9c390bf5837
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2021-01-29 22:37:07 +0700

    Create gayCommand.ts

---
+++ src/commands/Image/gayCommand.ts
@@ -0,0 +1,29 @@
+import { CommandConf } from "../../decorators";
+import Command from "../../handle/Command";
+import { MessageEmbed, MessageAttachment } from "discord.js";
+import type { Message } from "discord.js";
+
+@CommandConf({
+ name: "gay",
+ aliases: [],
+ description: "None",
+ usage: "gay [ @user || user id ]",
+ cooldown:3,
+ ownerOnly:false
+})
+
+export default class GayCommand extends Command {
+  public async exec(message: Message, args:string[]) {
+
+  let user = message.mentions.users?.first() || this.client.users.cache?.get(args[0]);
+  if(!user) {
+    user = message.author;
+  }
+
+  let img = await new this.client.image.Gay().getImage(user.displayAvatarURL({ format: "png", size: 2048 }));
+  let image = new MessageAttachment(img, "Gay.png");
+  let embed = new MessageEmbed()
+  return message.channel.send(image);
+
+ }
+}
