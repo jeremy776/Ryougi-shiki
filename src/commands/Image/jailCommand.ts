84fcf347de4688318b86cb2c22b911fad9e3c9b7
jeremy776 <62273120+jeremy776@users.noreply.github.com>
2021-01-30 08:21:06 +0700

    Create jailCommand.ts

---
+++ src/commands/Image/jailCommand.ts
@@ -0,0 +1,29 @@
+import { CommandConf } from "../../decorators";
+import Command from "../../handle/Command";
+import { MessageEmbed, MessageAttachment } from "discord.js";
+import type { Message } from "discord.js";
+
+@CommandConf({
+ name: "jail",
+ aliases: ["penjara"],
+ description: "None",
+ usage: "jail [ @user || user id ]",
+ cooldown:3,
+ ownerOnly:false
+})
+
+export default class JailCommand extends Command {
+  public async exec(message: Message, args:string[]) {
+
+  let user = message.mentions.users?.first() || this.client.users.cache?.get(args[0]);
+  if(!user) {
+    user = message.author;
+  }
+
+  let img = await new this.client.image.Jail().getImage(user.displayAvatarURL({ format: "png", size: 2048 }));
+  let image = new MessageAttachment(img, "Jail.png");
+  let embed = new MessageEmbed()
+  return message.channel.send(image);
+
+ }
+}
