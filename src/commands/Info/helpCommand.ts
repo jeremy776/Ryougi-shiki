d22647aec0ec367bb3cd1b51c35f17219f798e86
jeremy776 <jeremy.kusuma2018@gmail.com>
2021-01-31 20:00:46 +0700

    y

--- src/commands/Info/helpCommand.ts
+++ src/commands/Info/helpCommand.ts
@@ -62,7 +62,7 @@ export default class helpCommand extends Command {
             for (const category of categories) {
                 const commands = this.client.commands.filter(x => x.config.category === category);
                 embed.fields?.push({
-                    name: `${emote[category]}・${category} [\`${commands.map((x:any) => x.config.name).length}\`]`,
+                    name: `${category} [\`${commands.map((x:any) => x.config.name).length}\`]`,
                     value: commands.map((x:any) => `\`${x.config.name}\``).join(", ")
                 });
             }
