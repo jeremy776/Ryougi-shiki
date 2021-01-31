bd8db2734e9cf0d99a89d69f27c1dd1481c308fe
jeremy776 <jeremy.kusuma2018@gmail.com>
2021-01-31 12:38:45 +0700

    y

---
+++ src/commands/Game/mobile-legendsCommand.ts
@@ -0,0 +1,38 @@
+import { MessageEmbed } from "discord.js";
+const axios = require("axios");
+import type { Message } from "discord.js";
+import { CommandConf } from "../../decorators";
+import Command from "../../handle/Command";
+
+@CommandConf({
+ name: "mobile-legends",
+ aliases: ["ml", "mobileLegends"],
+ description: "See hero statistics on mobile legends",
+ usage: "mobile-legends [hero name]",
+ cooldown: 1,
+ ownerOnly:false
+})
+
+export default class MobileLegendsCommand extends Command {
+  public async exec(msg:Message, args:string[]) {
+    
+    let namaHero = args[0];
+    if(!namaHero) {
+      return msg.reply(`Invalid hero name.\n\`Example: ${this.client.prefix}mobile-legends Alpha\``)
+    }
+    
+    if(!isNAN(namaHero)) {
+      return msg.reply(`It seems that there is no hero name that only uses numbers`)
+    }
+    
+    let data = await this.client.util.getHero(namaHero);
+    
+    let embed = new MessageEmbed()
+    .setColor(this.client.color)
+    .setAuthor("Mobile Legends Hero " + namaHero.toUpperCase(), msg.author?.displayAvatarURL({dynamic:true}) as any)
+    .setThumbnail("https:"+data.key)
+    .setTimestamp()
+    .
+    
+  }
+}
