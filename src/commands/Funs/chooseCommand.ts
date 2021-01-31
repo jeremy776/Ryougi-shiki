6b531ceaa1f2ded79972214d7fbb007ea484c197
jeremy776 <jeremy.kusuma2018@gmail.com>
2021-02-01 00:19:18 +0700

    add new file

---
+++ src/commands/Funs/chooseCommand.ts
@@ -0,0 +1,30 @@
+import type {
+  Message
+} from "discord.js";
+import {
+  CommandConf
+} from "../../decorators";
+import Command from "../../handle/Command";
+
+@CommandConf({
+  name: "choose",
+  usage: "choose [me | they | he ]",
+  cooldown: 2,
+  aliases: [],
+  description: "the bot will select the random that was created, use | to make it into 2 choices"
+  ownerOnly: false
+})
+
+export default class ChooseCommand extends Command {
+  public async exec(message: Message, args:string[]) {
+    
+    let filtered = args.replace(/@everyone/g, "Unmentionable [\`everyone\`]").replace(/@here/g, "Unmentionable [\`here\`]");
+    let selection = filtered.split(" | ");
+    if(!selection) {
+      return message.reply(`Try using it like this for an example:\n\`${this.client.config.prefix}choose buy android | buy a car | buy a motorbike`);
+    }
+    
+    let random1 = Math.floor(Math.random() * selection.length);
+    return message.reply(`I chose: \`${selection[random1]}\``)
+  }
+}
