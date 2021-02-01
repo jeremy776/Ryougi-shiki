9ec7e0838322bf7cfc6d0eba2c2d2276f805c6dd
jeremy776 <jeremy.kusuma2018@gmail.com>
2021-02-01 16:00:44 +0700

    update command lgi

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 ## DATABASE
 + welcome.${guild.id}
++ leave.${guild.id}
 + autorole.${guild.id}
 + warning${guild.id}.${user.id}
-#### LEVELING DATABASE
+### LEVELING DATABASE
 - levelreward.${msg.guild?.id}
 - level-status.${msg.guild?.id}
 - level${msg.guild?.id}.${msg.author?.id}
---
+++ src/commands/Moderation/configCommand.ts
@@ -0,0 +1,61 @@
+import { CommandConf } from "../../decorators";
+import Command from "../../handle/Command";
+import type { Message } from "discord.js";
+import { MessageEmbed } from "discord.js";
+
+@CommandConf({
+  name: "config",
+  cooldown: 2,
+  aliases: [],
+  ownerOnly: false,
+  description: "see the guild config",
+  usage: "config"
+});
+
+export default class ConfigCommand extends Command {
+  public async exec(msg:Message, args:string[]) {
+    
+    let dataWelcome = await this.client.db.get(`welcome.${msg.guild.id}`);
+    let dataLeave = await this.client.db.get(`leave.${msg.guild.id}`);
+    if(!dataWelcome) {
+      dataWelcome = {
+        id: "NOT SET",
+        serverId: msg.guild?.id,
+        message: "NOT SET",
+        status: false,
+        bg: "NOT SET"
+      }
+    }
+    if(!dataLeave) {
+      dataLeave = {
+        id: "NOT SET",
+        serverId: msg.guild?.id,
+        message: "NOT SET",
+        status: false,
+        bg: "NOT SET"
+      }
+    }
+    
+    /* WELCOME */
+    let status;
+    if(dataWelcome.status == true) {
+      status = "ON"
+    }else{
+      status = "OFF"
+    }
+    let channel;
+    if(!isNaN(dataWelcome.id)) {
+      channel = `<#${dataWelcome.id}>`
+    }else{
+      channel = "NOT SET"
+    }
+    /* AKHIR WELCOME */
+    
+    let embed = new MessageEmbed()
+    .setAuthor(msg.guild?.name + " Config", msg.author?.displayAvatarURL({ dynamic:true }) as any)
+    .setColor(this.client.color)
+    .addField("WELCOME", `**STATUS:** ${status}\n**MESSAGE:** ${dataWelcome.message}\n**CHANNEL:** ${channel}`)
+    .setFooter("replied from "+msg.author?.username)
+    return msg.channel.send(embed)
+  }
+}
